1676995be159824544a8ee02c3b1fe45d2865163
Abdurrahman-Bres <babdo@protonmail.com>
2020-08-06 04:42:09 +0200

    add code

---
+++ Project_X/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 02:27:36 2020
+
+@author: abdo
+"""
+
+import numpy as np
+
+
+def main():
+    x = np.random.rand(1)[0]
+    print('This is project X', str(x)[-2:])
+
+
+if __name__ == '__main__':
+    main()
---
+++ build.sh
@@ -0,0 +1,2 @@
+
+sudo docker build -t python-image-01 -f ./docker-build-folder/Dockerfile .
